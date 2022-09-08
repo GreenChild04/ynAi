ae3bde2ec1c1b67ed40930e395de812ee66641c4
GreenChild04 <106739622+GreenChild04@users.noreply.github.com>
2022-09-08 22:12:04 +1000

    AI: Finished V2
    I have finished the ai v2 with word links storing weights instead of words themselves to produce a more personalised experience for the generations.

--- yn/main.py
+++ yn/main.py
@@ -1,11 +1,15 @@
 from memory import Memory
+from tqdm import tqdm;
 
 
 def train(rounds):
     trainer = Memory().load();
+    bar = tqdm(total=rounds, position=0);
+    bar.set_description("Training ai with data...");
     data = open("training.dat").read().split("\n");
 
     for i in range(rounds):
+        bar.update()
         for a in data:
             trainer.train(a);
             trainer.store();
@@ -20,4 +24,4 @@ def run():
 
 
 if __name__ == "__main__":
-    train(100);
+    run()
--- yn/memory.py
+++ yn/memory.py
@@ -3,20 +3,91 @@ import pickle
 from random import random
 
 
-@dataclass()
 class Word:
-    memory: vars;
-    name: str;
-    weight: float = 1.0;
+    def __init__(self, memory, name, links=None):
+        self.links = [];
+        if links:
+            self.links = links
+        self.memory = memory;
+        self.name = name;
+
+    def append(self, link):
+        if isinstance(link, Word):
+            for i in link.links:
+                self.append(i);
+        elif isinstance(link, WordLink):
+            self.delDupe(link.id);
+            self.links.append(link);
+        else:
+            raise Exception("Cannot Append This Object to Word")
+
+    def vAdd(self, value, words):
+        for i in self.links:
+            if i.other in words:
+                i.weight += value;
+                if i.weight > 1.9:
+                    i.weight = 1.9;
+                elif i.weight < 0.1:
+                    i.weight = 0.1;
+
+    def delDupe(self, id):
+        for i in self.links:
+            if i.id == id:
+                self.links.remove(i);
+
+    def toId(self):
+        names = [];
+        for i in self.links:
+            names.append(i.id);
+        return names;
+
+    def toName(self):
+        names = [];
+        for i in self.links:
+            names.append(i.other.name);
+        return names;
+
+    def new(self, other):
+        weights = [];
+        for i in self.links:
+            weights.append(i.weight);
+        weight = self.memory.avg(weights);
+        self.links.append(WordLink(self, other, weight));
+        return self
+
+    def toWeight(self, words):
+        weights = [];
+        word = [];
+        for a in words:
+            word.append(a.name);
+        for i in words:
+            if i.name not in self.toName():
+                self.new(i);
+        for i in self.links:
+            if i.other.name in word and i.other.name != self.name:
+                # print(i);
+                weights.append(i.weight);
+        return weights;
+
+    def find(self, name):
+        link = None
+        for i in self.links:
+            if i.id == name:
+                link = i;
+        return link
 
     def copy(self):
-        return Word(self.memory, self.name, self.weight);
+        return Word(self.memory, self.name, self.links);
 
 
 @dataclass()
 class WordLink:
     origin: vars
     other: vars
+    weight: float = 1.0
+
+    def __post_init__(self):
+        self.id = f"[{self.origin.name}]:[{self.other.name}]";
 
     def set(self, this):
         self.origin = this;
@@ -24,6 +95,9 @@ class WordLink:
 
     def copy(self):
         return WordLink(self.origin, self.other);
+
+    def __repr__(self):
+        return f"\t({self.id}:[{self.weight}])";
 
 
 class WordList:
@@ -41,16 +115,16 @@ class WordList:
         else:
             raise Exception("Cannot Append Non WordType Object!");
 
-    def vAdd(self, value):
-        for i in self.words:
-            i.weight += value;
-
     def find(self, wordName):
         word = None;
         for i in self.words:
             if i.name == wordName:
                 word = i;
         return word;
+
+    def vAdd(self, value):
+        for i in self.words:
+            i.vAdd(value, self.words);
 
     def delDupe(self, name):
         for i in self.words:
@@ -66,7 +140,8 @@ class WordList:
     def toWeight(self):
         weights = [];
         for i in self.words:
-            weights.append(i.weight);
+            for a in i.toWeight(self.words):
+                weights.append(a);
         return weights;
 
     def inherit(self, other, names=None):
@@ -93,7 +168,7 @@ class Memory:
             return "No";
 
     def train(self, inpit):
-        raw = inpit.lower().strip("?").split(" ");
+        raw = inpit.lower().strip("?").split(";")[0].split(" ");
         real = int(inpit.strip(" ").split(";")[1])
         words = self.generate(raw);
         out = self.randomise(words);
@@ -118,10 +193,12 @@ class Memory:
 
     def randomise(self, words):
         weights = words.toWeight();
-        print(weights)
         avg = self.avg(weights);
         trueRan = random();
+        if trueRan < 0.1:
+            trueRan = 0.1;
         comp = trueRan * avg;
+        print(f"\taverage[{avg}] random[{trueRan}] final[{comp}]")
         comp = round(comp);
         return comp;
 
@@ -129,7 +206,10 @@ class Memory:
         out = 0;
         for i in num:
             out += i;
-        out /= len(num);
+        try:
+            out /= len(num);
+        except:
+            out = 1.0
         return out
 
     def store(self):
